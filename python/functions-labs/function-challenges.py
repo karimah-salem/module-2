cdd9dcdae0dee9f8eb2c3fbc4f90a749f3b7bcb2
karimah-salem <karimah.salem@gmail.com>
2024-03-14 12:10:18 +0000

    2

---
+++ python/functions-labs/function-challenges.py
@@ -0,0 +1,31 @@
+#challenge 1
+#def password_checker():
+    #saved_passes = ["password1", "password2", "abcd123"]
+
+    #check_pass = input("Please enter your password: ")
+    
+   # if check_pass in saved_passes:
+        #print(f"Use a safer password '{check_pass}' is compromised")
+    #else:
+  #      print("password is safe")
+
+#password_checker()
+
+
+#challenge2
+
+def calculate():
+    
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+
+    add_sum = num1 + num2
+    sub_sum = num1 - num2
+    multi_sum = num1 * num2
+    
+    print(f"add is {add_sum}, subtraction is {sub_sum}, multiplication is {multi_sum}")
+
+
+calculate()
+
+#challenge3
