2034a19ba6897cab02a44c216fc345d1f3b51917
karimah-salem <karimah.salem@gmail.com>
2024-03-15 11:44:47 +0000

    2

---
+++ python/classes-labs/labs-1.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+student1 = Student("john", 19)
+student2 = Student("tom", 21)
+
+print(f"The age of {student1.name} is {student1.age} years.")
+
--- python/functions-labs/function-challenges.py
+++ python/functions-labs/function-challenges.py
@@ -14,18 +14,52 @@
 
 #challenge2
 
-def calculate():
+#def calculate():
     
-    num1 = int(input("Enter a number: "))
-    num2 = int(input("Enter another number: "))
+    #num1 = int(input("Enter a number: "))
+    #num2 = int(input("Enter another number: "))
 
-    add_sum = num1 + num2
-    sub_sum = num1 - num2
-    multi_sum = num1 * num2
+    #add_sum = num1 + num2
+    #sub_sum = num1 - num2
+    #multi_sum = num1 * num2
     
-    print(f"add is {add_sum}, subtraction is {sub_sum}, multiplication is {multi_sum}")
+#    print(f"add is {add_sum}, subtraction is {sub_sum}, multiplication is {multi_sum}")
 
 
-calculate()
+#calculate()
+
+
 
 #challenge3
+
+#def max_num(num1, num2, num3):
+    #highest = max(num1, num2, num3)
+    #return highest
+
+#result = max_num(29, 8, 4)
+#print(result)  
+
+
+#challenge4
+
+#def check_odd_even(num):
+  #if num % 2 == 0 :
+    #return (f"number {num} is even.")
+  #else:
+    #return (f"number {num} is odd.")
+  
+#entry_num = int(input("enter a number: "))
+
+#result = check_odd_even(entry_num)
+#print(result)
+
+
+#challenge5
+
+def convert_upper(word):
+  return word.upper()
+
+user_input =("enter a word: ")
+
+result = convert_upper(user_input)
+print("uppercase:" , result)
